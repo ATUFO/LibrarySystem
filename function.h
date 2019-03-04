a6e211f59b29e53d04ce429cdb65a3bcfe5dfc72
TF <2221057667@qq.com>
2019-03-04 19:21:50 +0800

    headspilt

--- function.h
+++ function.h
@@ -6,8 +6,5 @@
 
 void userline_Init();//用户链表初始化
 void bookline_Init();//初始化
-
 void welcome();//欢迎界面
-
-
 #endif // FUNCTION_H_INCLUDED
--- menu.h
+++ menu.h
@@ -5,8 +5,6 @@
 
 #endif // MENU_H_INCLUDED
 void menu();//菜单
-
-
 void userMenu();//用户菜单
 void adminMenu();//管理员菜单
 void userAdminMenu();//用户管理菜单
